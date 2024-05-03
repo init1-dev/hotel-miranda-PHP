b39ffb965df7d60384108bea74146f4a3939bd90
init1-dev <init1.dev@gmail.com>
2024-05-03 11:18:47 +0200

    REMOVE: bladeOne.php, ADD: bladeOne package using composer, FIX: setup.php into renderTemplate

--- utils/renderTemplate.php
+++ utils/renderTemplate.php
@@ -1,5 +1,12 @@
 <?php
-    require_once __DIR__ . '/setup.php';
+    require_once __DIR__ . '/../vendor/autoload.php';
+    
+    use eftec\bladeone\BladeOne;
+
+    $views = __DIR__ . '/../views';
+    $cache = __DIR__ . '/../cache';
+    
+    $blade = new BladeOne($views, $cache, BladeOne::MODE_DEBUG);
     
     function renderTemplate($template, $values) {
         global $blade;
