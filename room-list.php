17b6592d2e076ff517d059df407d678b6a481578
init1-dev <init1.dev@gmail.com>
2024-05-03 18:13:38 +0200

    ADD: executeQueryWithParams, FIX: refactor room-list

--- room-list.php
+++ room-list.php
@@ -4,48 +4,21 @@
     require_once __DIR__ . '/utils/queries/checkAvailability.php';
     require_once __DIR__ . '/utils/room/formRoomData.php';
     require_once __DIR__ . '/utils/room/getAmenity.php';
+    require_once __DIR__ . '/utils/executeQueryWithParams.php';
 
-    if($stmt = $connection->prepare($checkAvailability)){
-        $checkArrival = isset($_GET['check_in']);
-        $checkDeparture = isset($_GET['check_out']);
-        
-        if( $checkArrival && $checkDeparture){
-            $arrival = $_GET['check_in'];
-            $departure = $_GET['check_out'];
-        } else{
-            echo "Invalid params";
-            exit;
-        }
+    $arrival = isset($_GET['check_in']) ? $_GET['check_in'] : null;
+    $departure = isset($_GET['check_out']) ? $_GET['check_out'] : null;
 
-        $stmt->bind_param('ss', $departure, $arrival);
+    $availableRooms = executeQueryWithParams($connection, $checkAvailability, 'ss', [
+        $departure,
+        $arrival
+    ]);
 
-        if($stmt->execute()){
-            $results = $stmt->get_result();
-            $availableRooms = [];
-
-            if($results->num_rows > 0){
-                while ($row = $results->fetch_assoc()){
-                    $availableRooms[] = $row;
-                }
-            } else {
-                echo "No results";
-                exit;
-            }
-        } else{
-            echo "Error running query: " . $connection->error;
-            exit;
-        }
-
-        $stmt->close();
-
-        $formatedRooms = formRoomData($availableRooms);
-        
-    } else {
-        echo "Unable to GET data";
-        exit;
-    }
+    echo count($availableRooms);
 
     $connection->close();
+    
+    $formatedRooms = formRoomData($availableRooms);
 
     $values = [
         'rooms' => $formatedRooms,
---
+++ utils/executeQueryWithParams.php
@@ -0,0 +1,31 @@
+<?php
+
+    function executeQueryWithParams($connection, $query, $paramsType, $params = [], $close = true) {
+        $stmt = $connection->prepare($query);
+        $stmt->bind_param($paramsType, ...$params);
+
+        if($stmt->execute()){
+            $results = $stmt->get_result();
+            $data = [];
+
+            if($results->num_rows > 0){
+                while ($row = $results->fetch_assoc()){
+                    $data[] = $row;
+                }
+            } else {
+                echo "No results";
+                exit;
+            }
+        } else{
+            echo "Error running query: " . $connection->error;
+            exit;
+        }
+
+        if($close){
+            $stmt->close();
+        }
+
+        return $data;
+    }
+
+?>
