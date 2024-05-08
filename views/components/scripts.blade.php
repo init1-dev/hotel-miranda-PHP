8519013a54d974e5782f578391146e745f1a1fb6
init1-dev <init1.dev@gmail.com>
2024-05-08 17:41:40 +0200

    FIX: main.js script defer

--- views/components/scripts.blade.php
+++ views/components/scripts.blade.php
@@ -1,5 +1,5 @@
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js" defer></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer" defer></script>
 <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11.10.8/dist/sweetalert2.all.min.js'></script>
-<script type="module" src="./js/main.js"></script>
+<script type="module" src="./js/main.js" defer></script>
 <script type="module" src="./js/swipers.js" defer></script>
